23d1c678392221a3800791061c4e73dbe6fdd11f
Hanz Luo <lhz0516@hotmail.com>
2017-07-28 22:32:16 -0700

    use es7 async for mutation

--- imports/api/server/graphql/resolvers/contacts.js
+++ imports/api/server/graphql/resolvers/contacts.js
@@ -1,9 +1,9 @@
 export default {
   Mutation: {
-    submitContact: Meteor.bindEnvironment(({ dbs }, { input }) => {
+    async submitContact({ dbs }, { input }) {
       input.createdAt = new Date();
       const _id = dbs.Contacts.insert(input);
       return { _id };
-    })
+    }
   }
 };
