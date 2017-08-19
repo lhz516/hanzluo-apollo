e7e354f117de1d4c7b2ac37c5eb13f5f676d8506
Hanz Luo <lhz0516@hotmail.com>
2017-08-19 02:56:26 -0700

    show loading icon when ssr d3 cloud

--- imports/modules/home/react-d3-cloud/index.js
+++ imports/modules/home/react-d3-cloud/index.js
@@ -1,11 +1,13 @@
 // Forked from https://github.com/Yoctol/react-d3-cloud
 
+import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Component } from 'react';
 import PropTypes from 'prop-types';
 import ReactFauxDom from 'react-faux-dom';
 import * as d3 from 'd3';
 import cloud from 'd3-cloud';
+import Icon from 'antd/lib/icon'
 
 const defaultFontSizeMapper = word => word.value;
 const fill = d3.scaleOrdinal(d3.schemeCategory10);
@@ -52,6 +54,10 @@ class WordCloud extends Component {
     this.wordCloud.style.setProperty('width', '100%');
     this.wordCloud.style.setProperty('height', '100%');
     this.wordCloud.style.setProperty('margin', '0 auto');
+    if (Meteor.isServer) {
+      this.wordCloud.style.setProperty('text-align', 'center');
+      this.wordCloud.appendChild(<Icon key="1" style={{ fontSize: '36px', marginTop: '60px' }} type="loading" />)
+    }
   }
 
   componentDidMount() {
